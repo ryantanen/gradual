f5da6ad2e41f38c52b1c779040b68917600557c3
Ryan Tanenholz <44756861+ryantanen@users.noreply.github.com>
2025-03-29 11:13:35 -0400

    Auth Init

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Auth0Provider
+      domain={process.env.REACT_APP_AUTH0_DOMAIN as string}
+      clientId={process.env.REACT_APP_AUTH0_CLIENT_ID as string}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+        audience: process.env.REACT_APP_AUTH0_AUDIENCE as string,
+      }}
+    >
+      <App />
+    </Auth0Provider>
+  </StrictMode>
+);
---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,8 @@
+import LoginButton from "@/components/ui/loginButton";
+import React from "react";
+
+function Login() {
+  return <LoginButton></LoginButton>;
+}
+
+export default Login;
