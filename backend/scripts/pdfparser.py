f07e32251fd111dcc81e6aa73e67c5180209b659
Yuvraj <11yuviji@gmail.com>
2025-03-29 23:57:38 -0400

    modal init

--- backend/scripts/pdfparser.py
+++ backend/scripts/pdfparser.py
@@ -1,5 +1,5 @@
 import pdfplumber
 
-with pdfplumber.open("data/sample.pdf") as pdf:
+with pdfplumber.open("uploads/sample.pdf") as pdf:
     first_page = pdf.pages[0]
     print(first_page.extract_text())
